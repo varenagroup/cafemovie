ad08195b567f468c5d3c0ca43fd69844edbf808c
Mr.Majid <majid.mehdizadeh16@gmail.com>
2024-02-11 18:44:14 +0330

    create project

---
+++ app/src/main/java/ir/teaching/cafemovie/MainActivity.kt
@@ -0,0 +1,18 @@
+package ir.teaching.cafemovie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ir.teaching.cafemovie.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/ir/teaching/cafemovie/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package ir.teaching.cafemovie.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
